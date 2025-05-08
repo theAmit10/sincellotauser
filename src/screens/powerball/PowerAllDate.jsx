9cc6657abd994d5d8a3a8d4b479bf2f32e30bb38
theAmit10 <prasadak66@gmail.com>
2025-05-08 13:48:09 +0530

    showing three date for power insight

--- src/screens/powerball/PowerAllDate.jsx
+++ src/screens/powerball/PowerAllDate.jsx
@@ -1,16 +1,12 @@
 import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import MainBackgroundWithoutScrollview from '../../components/background/MainBackgroundWithoutScrollview';
-import PowerAllTimesComp from '../../components/powerball/poweralltimes/PowerAllTimesComp';
-import {heightPercentageToDP} from 'react-native-responsive-screen';
 import Loading from '../../components/helpercComponent/Loading';
-import {COLORS, FONT} from '../../../assets/constants';
 import {useNavigation} from '@react-navigation/native';
 import PowerballAllDateComp from '../../components/powerball/powerballalldates/PowerballAllDateComp';
 import {useGetPowerDateBasedUponPowerTimeQuery} from '../../helper/Networkcall';
 import {useSelector} from 'react-redux';
-import moment from 'moment';
-
+import moment from 'moment-timezone';
 const PowerAllDate = ({route}) => {
   const navigation = useNavigation();
 
@@ -25,18 +21,22 @@ const PowerAllDate = ({route}) => {
 
   const {accesstoken} = useSelector(state => state.user);
 
-  const {isLoading, data, isError} = useGetPowerDateBasedUponPowerTimeQuery({
+  const {
+    isLoading,
+    data: dateData,
+    isError,
+  } = useGetPowerDateBasedUponPowerTimeQuery({
     accesstoken,
     id: powertime._id,
     page,
     limit,
   });
 
-  useEffect(() => {
-    if (!isLoading && data) {
-      setPowerDate(data.powerDates);
-    }
-  }, [isLoading, data]);
+  // useEffect(() => {
+  //   if (!isLoading && data) {
+  //     setPowerDate(data.powerDates);
+  //   }
+  // }, [isLoading, data]);
 
   // useEffect(() => {
   //   if (data) {
@@ -52,6 +52,43 @@ const PowerAllDate = ({route}) => {
   //     setPowerDate(filtered); // Update filteredData whenever dates change
   //   }
   // }, [data]);
+
+  useEffect(() => {
+    if (dateData) {
+      // Get current, previous, and next dates
+      const currentDate = moment().startOf('day');
+      const previousDate = currentDate.clone().subtract(1, 'days');
+      const nextDate = currentDate.clone().add(1, 'days');
+
+      // Get the current time
+      const currentTime = moment();
+      const isWithinTimeRange = currentTime.isBetween(
+        moment().set({hour: 23, minute: 0}), // 11:00 PM
+        moment().add(1, 'days').startOf('day'), // 12:00 AM
+      );
+
+      const filtered = dateData?.powerDates.filter(item => {
+        const itemDate = moment(item.powerdate, 'DD-MM-YYYY');
+
+        // Always include the previous date and current date
+        if (
+          itemDate.isSame(previousDate, 'day') ||
+          itemDate.isSame(currentDate, 'day')
+        ) {
+          return true;
+        }
+
+        // Include the next date if it's between 11:00 PM - 12:00 AM
+        if (itemDate.isSame(nextDate, 'day') && isWithinTimeRange) {
+          return true;
+        }
+
+        return false; // Exclude all other dates
+      });
+
+      setPowerDate(filtered); // Update the filtered data with the three dates
+    }
+  }, [dateData]);
 
   return (
     <MainBackgroundWithoutScrollview title={'All Date'}>
