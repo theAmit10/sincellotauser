c837653c2aa44dd5720b07f69d8942f981c26ffd
theAmit10 <prasadak66@gmail.com>
2025-05-23 17:41:22 +0530

    sign up process updated

--- src/components/login/AdminAuthLoginBackground.jsx
+++ src/components/login/AdminAuthLoginBackground.jsx
@@ -1,18 +1,12 @@
-
-
-
-
-
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import {
   heightPercentageToDP,
   widthPercentageToDP,
 } from 'react-native-responsive-screen';
-import { COLORS, FONT } from '../../../assets/constants';
-import { useNavigation } from '@react-navigation/native';
-
+import {COLORS, FONT} from '../../../assets/constants';
+import {useNavigation} from '@react-navigation/native';
 
 const AdminAuthLoginBackground = () => {
   const navigation = useNavigation();
@@ -22,7 +16,7 @@ const AdminAuthLoginBackground = () => {
   const navigateToRegister = () => {
     console.log('count :: ' + countVal);
     setCountVal(countVal + 1);
-    if (countVal >= 5) {
+    if (countVal >= 20) {
       navigation.navigate('Register');
     }
   };
@@ -33,85 +27,80 @@ const AdminAuthLoginBackground = () => {
         flex: 1,
         backgroundColor: COLORS.grayBg,
       }}>
-
-        <ImageBackground
-        source={require("../../../assets/image/tlwbg.jpg")}
+      <ImageBackground
+        source={require('../../../assets/image/tlwbg.jpg')}
         style={{
           width: '100%',
           height: '100%',
-        }}
-        >
-
-        
+        }}>
         {/** Top View Rectangle View */}
-      <View
-        style={{
-          width: heightPercentageToDP(30),
-          height: heightPercentageToDP(30),
-          backgroundColor: COLORS.white_s,
-          position: 'absolute',
-          borderRadius: heightPercentageToDP(5),
-          zIndex: 1,
-          top: heightPercentageToDP(10),
-          left: widthPercentageToDP(20),
-        }}>
         <View
           style={{
-            width: heightPercentageToDP(15),
+            width: heightPercentageToDP(30),
             height: heightPercentageToDP(30),
-            backgroundColor: COLORS.lightWhite,
+            backgroundColor: COLORS.white_s,
             position: 'absolute',
+            borderRadius: heightPercentageToDP(5),
             zIndex: 1,
-            borderTopLeftRadius: heightPercentageToDP(5),
-            borderBottomLeftRadius: heightPercentageToDP(5),
-          }}></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'rgba(128, 128, 128, 0.5)',
-          width: widthPercentageToDP(50),
-          flex: 1,
-          opacity: 80,
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-            gap: heightPercentageToDP(0.5),
+            top: heightPercentageToDP(10),
+            left: widthPercentageToDP(20),
           }}>
-          <Text
-            onPress={navigateToRegister}
+          <View
             style={{
-              fontFamily: FONT.ZCOOL_Regular,
-              fontSize: heightPercentageToDP(2.5),
-              paddingVertical: heightPercentageToDP(2),
-              paddingStart: heightPercentageToDP(2),
-              color: COLORS.white_s,
-            }}>
-            Since
-          </Text>
-          <Text
-            style={{
-              fontFamily: FONT.ZCOOL_Regular,
-              fontSize: heightPercentageToDP(2.5),
-              paddingVertical: heightPercentageToDP(2),
-              paddingEnd: heightPercentageToDP(2),
-              color: COLORS.white_s,
-            }}>
-            2001
-          </Text>
+              width: heightPercentageToDP(15),
+              height: heightPercentageToDP(30),
+              backgroundColor: COLORS.lightWhite,
+              position: 'absolute',
+              zIndex: 1,
+              borderTopLeftRadius: heightPercentageToDP(5),
+              borderBottomLeftRadius: heightPercentageToDP(5),
+            }}></View>
         </View>
-
         <View
-          className="rounded-full h-5 w-5"
           style={{
-            margin: heightPercentageToDP(3),
-            backgroundColor: COLORS.background,
-          }}></View>
-      </View>
+            backgroundColor: 'rgba(128, 128, 128, 0.5)',
+            width: widthPercentageToDP(50),
+            flex: 1,
+            opacity: 80,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              gap: heightPercentageToDP(0.5),
+            }}>
+            <Text
+              onPress={navigateToRegister}
+              style={{
+                fontFamily: FONT.ZCOOL_Regular,
+                fontSize: heightPercentageToDP(2.5),
+                paddingVertical: heightPercentageToDP(2),
+                paddingStart: heightPercentageToDP(2),
+                color: COLORS.white_s,
+              }}>
+              Since
+            </Text>
+            <Text
+              style={{
+                fontFamily: FONT.ZCOOL_Regular,
+                fontSize: heightPercentageToDP(2.5),
+                paddingVertical: heightPercentageToDP(2),
+                paddingEnd: heightPercentageToDP(2),
+                color: COLORS.white_s,
+              }}>
+              2001
+            </Text>
+          </View>
+
+          <View
+            className="rounded-full h-5 w-5"
+            style={{
+              margin: heightPercentageToDP(3),
+              backgroundColor: COLORS.background,
+            }}></View>
+        </View>
       </ImageBackground>
-      
     </View>
   );
 };
@@ -119,7 +108,6 @@ const AdminAuthLoginBackground = () => {
 export default AdminAuthLoginBackground;
 
 const styles = StyleSheet.create({});
-
 
 // import {Text, View} from 'react-native';
 // import React, {useState} from 'react';
@@ -221,5 +209,3 @@ const styles = StyleSheet.create({});
 // };
 
 // export default AdminAuthLoginBackground;
-
-
